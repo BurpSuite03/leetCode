c0471cf2864d9a20bef18faac7e3e54514fda0ea
dtc <15948311472@163.com>
2019-12-10 18:50:52 +0800

    取最长字串

---
+++ src/LeetCode/LeetCode03.java
@@ -0,0 +1,19 @@
+package LeetCode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Description 给定一个字符串，查出其中不含重复字符的最长字串
+ * @author Administrator
+ *
+ */
+public class LeetCode03 {
+	public static void main(String args[]) {
+		String s = "gjfjoqweomgmighojqagjoihdfjgwkejkghjrgjnburyhuljjg";
+		Map<String, Integer> map = new HashMap<>();
+		for(int i=0; i<s.length()-1; i++) {
+			
+		}
+	}
+}
